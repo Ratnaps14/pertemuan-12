dd45e66fff13e3d05427cbbbe70916f3a91619ad
DESKTOP-70IG8ER\Lab-ERP-38 <ratnapuspitasarii56@gmail.com>
2022-11-13 16:50:37 +0700

    pertemuan12

--- app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
+++ app/src/main/java/com/chirikualii/materidb/ui/search/SearchActivity.kt
@@ -2,9 +2,12 @@ package com.chirikualii.materidb.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Adapter
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.core.widget.doOnTextChanged
+import androidx.lifecycle.ViewModelProvider
 import com.chirikualii.materidb.data.model.Movie
 import com.chirikualii.materidb.databinding.ActivitySearchBinding
 import com.chirikualii.materidb.ui.adapter.MovieListAdapter
@@ -13,48 +16,33 @@ class SearchActivity : AppCompatActivity() {
 
     private lateinit var listAdapter: MovieListAdapter
     private lateinit var binding: ActivitySearchBinding
+
+   val viewModel : SearchViewModel by viewModels<SearchViewModel>(
+        factoryProducer = { SearchViewModelFactory(context = this)}
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        observeView()
         listAdapter = MovieListAdapter()
         binding.rvMovie.adapter=listAdapter
-            listAdapter.addItem(
-            listOf(
-                Movie(
-                    movieId = "kk",
-                    title = "As",
-                    releaseDate = "asdasd",
-                    imagePoster = "asda",
-                    backdrop = "sda",
-                    overview = "sdas",
-                    bookmark = 1),
 
-                Movie(
-                movieId = "kk",
-                title = "As",
-                releaseDate = "asdasd",
-                imagePoster = "asda",
-                backdrop = "sda",
-                overview = "sdas",
-                bookmark = 1),
-
-                Movie(
-                    movieId = "kk",
-                    title = "As",
-                    releaseDate = "asdasd",
-                    imagePoster = "asda",
-                    backdrop = "sda",
-                    overview = "sdas",
-                    bookmark = 1),
-
-                )
-
-
-        )
         binding.etSearch.doOnTextChanged { text,_,_,_ ->
+            viewModel.dosearchMovie(text.toString())
             Toast.makeText(this,"serching ${text}", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun observeView(){
+        viewModel.listMovie.observe(this) {
+            listAdapter.addItem(it)
+        }
+
+        viewModel.isLoading.observe(this){
+            if(it){
+                binding.progressBar.visibility = View.VISIBLE
+            }
         }
     }
 }
---
+++ app/src/main/java/com/chirikualii/materidb/ui/search/SearchViewModelFactory.kt
@@ -0,0 +1,21 @@
+package com.chirikualii.materidb.ui.search
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.chirikualii.materidb.data.local.MovieDbImpl
+import com.chirikualii.materidb.data.remote.ApiClient
+import com.chirikualii.materidb.data.repository.MovieRepoImpl
+
+class SearchViewModelFactory (val context :Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        val moviedbImpl = MovieDbImpl(context)
+        val repo = MovieRepoImpl(ApiClient.service,moviedbImpl)
+        val viewModel = SearchViewModel(repo)
+
+        return viewModel as T
+    }
+
+
+}
